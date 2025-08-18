1aa956155cde9e1b94c9cd9b20b4b52218b190c6
Alejandro <alejandroluisacosta95@gmail.com>
2025-08-18 14:12:30 +0200

    add: initial logic for unblocking a session from the EnterCode modal

--- frontend/src/Components/BlockedSessionModal/BlockedSessionModal.jsx
+++ frontend/src/Components/BlockedSessionModal/BlockedSessionModal.jsx
@@ -7,7 +7,7 @@ const BlockedSessionModal = ({ setOpenBlockedSessionModal, selectedSession, setS
     const handleUnblockSession = (id, code) => {
         setSessions(prev => prev.map(session => {
             let returnValue = {...session}
-            if (session.id === id) {
+            if (session.id === id && session.unblockCode === code) {
                 returnValue.isBlocked = false;
             }
             return returnValue;
@@ -17,7 +17,7 @@ const BlockedSessionModal = ({ setOpenBlockedSessionModal, selectedSession, setS
 
     return (
         <>
-            <EnterCodeModal />
+            <EnterCodeModal selectedSessionId={selectedSession.id} handleUnblockSession={handleUnblockSession}/>
             <div className="blocked-session-modal">
                 <img className="blocked-session-modal__close" src='/icons/close.svg' onClick={handleCloseBlockedSessionModal} alt="Cerrar pantalla de sesión bloqueada" />
                 <h3 className="blocked-session-modal__title">Desbloquear sesión #{selectedSession ? selectedSession.id : 0}</h3>
@@ -25,7 +25,7 @@ const BlockedSessionModal = ({ setOpenBlockedSessionModal, selectedSession, setS
                 <p className="blocked-session-modal__text">Escríbenos a <strong>pensamientosqueatormentan@gmail.com</strong> respondiéndo a la siguiente pregunta:</p>
                 <p className="blocked-session-modal__text">{selectedSession ? selectedSession.unblockQuestion : 0}</p>
                 <p className="blocked-session-modal__text">Nuestro equipo te dará un código para desbloquear esta sesión.</p>
-                <button className="blocked-session-modal__button" onClick={() => handleUnblockSession(selectedSession.id)}>YA TENGO EL CÓDIGO</button>
+                <button className="blocked-session-modal__button">YA TENGO EL CÓDIGO</button>
                 <hr className="blocked-session-modal__line"/>
                 <p className="blocked-session-modal__text">Opción 2: Compra el curso completo en Hotmart por 10€</p>
                 <button className="blocked-session-modal__button">COMPRAR</button>
--- frontend/src/Components/EnterCodeModal/EnterCodeModal.jsx
+++ frontend/src/Components/EnterCodeModal/EnterCodeModal.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import './EnterCodeModal.css'
 
-const EnterCodeModal = () => {
+const EnterCodeModal = ({ selectedSessionId, handleUnblockSession }) => {
+    const [userInput, setUserInput] = useState('');
+
+    const handleInputChange = ({ target }) => {
+        setUserInput(target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const input = userInput.trim();
+        handleUnblockSession(selectedSessionId, input)
+    }
+
     return (
-        <div className="enter-code-modal">
+        <form className="enter-code-modal" onSubmit={handleSubmit}>
             <img className="enter-code-modal__close-icon" src='/icons/close.svg' alt="Cerrar pantalla de sesión bloqueada" />
-            <label for="unblock-code-input" className="enter-code-modal__title">CÓDIGO</label>
-            <input type="text" id="unblock-code-input" className="enter-code-modal__input" />
-            <button className="enter-code-modal__button">DESBLOQUEAR</button>
+            <label htmlFor="unblock-code-input" className="enter-code-modal__title">CÓDIGO</label>
+            <input type="text" id="unblock-code-input" className="enter-code-modal__input" value={userInput} onChange={handleInputChange}/>
+            <button className="enter-code-modal__button" type="submit">DESBLOQUEAR</button>
             <p className="enter-code-modal__close-text">Cerrar</p>
-        </div>
+        </form>
     )
 }
 
