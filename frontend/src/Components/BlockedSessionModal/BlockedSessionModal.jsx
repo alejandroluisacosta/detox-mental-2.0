a6414bdf4562f75bbf1d0bb1aa0a6f9a9003c14d
Alejandro <alejandroluisacosta95@gmail.com>
2025-09-04 19:12:50 +0200

    add: blocked session modal fade in transition

--- frontend/src/Components/BlockedSessionModal/BlockedSessionModal.jsx
+++ frontend/src/Components/BlockedSessionModal/BlockedSessionModal.jsx
@@ -27,7 +27,7 @@ const BlockedSessionModal = ({ setOpenBlockedSessionModal, selectedSession, setS
     return (
         <>
             {openEnterCodeModal && <EnterCodeModal selectedSessionId={selectedSession.id} handleUnblockSession={handleUnblockSession} setOpenEnterCodeModal={setOpenEnterCodeModal}/>}
-            <div className="blocked-session-modal">
+            <div className="blocked-session-modal modal-fade-in">
                 <img className="blocked-session-modal__close" src='/icons/close.svg' onClick={handleCloseBlockedSessionModal} alt="Cerrar pantalla de sesión bloqueada" />
                 <h3 className="blocked-session-modal__title">Desbloquear sesión #{selectedSession ? selectedSession.id : 0}</h3>
                 <p className="blocked-session-modal__text">Opción 1 (Gratis):</p>
