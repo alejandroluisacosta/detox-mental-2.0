8df82ad630c572f8104425a19f8d00db2b44d8c9
Alejandro <alejandroluisacosta95@gmail.com>
2025-09-11 19:49:38 +0200

    add: session card component

---
+++ frontend/src/Components/SessionCard/SessionCard.jsx
@@ -0,0 +1,14 @@
+const SessionCard = ({ session, index, handleGoToSession }) => {
+    return (
+        <div key={index} className='session-card' onClick={() => handleGoToSession(session.id)}>
+            {session.isBlocked && <div className='session-card__blocked-layer'></div>}
+            <img src={session.img} className='session-card__image' alt={`Imagen de sesión ${session.id}`}/>
+            <h2 className='session-card__header'>{`Sesión ${session.id}:`}</h2>
+            <h2 className='session-card__title'>{session.title}</h2>
+            <p className='session-card__description'>{session.description}</p>
+            <button className={`session-card__button${session.isBlocked ? ' session-card__button--blocked' : ''}`}>{session.isBlocked ? 'DESBLOQUEAR' : 'ESCUCHAR'}</button>
+        </div>
+    )
+}
+
+export default SessionCard;
