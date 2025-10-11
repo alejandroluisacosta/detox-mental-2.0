05b9434abc72c0b75ef8a7b0d71db5efaa890c88
Alejandro <alejandroluisacosta95@gmail.com>
2025-10-11 21:55:51 +0200

    add: unblock elements

--- frontend/src/Components/ExerciseModal/ExerciseModal.jsx
+++ frontend/src/Components/ExerciseModal/ExerciseModal.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import CloseIcon from '../CloseIcon/CloseIcon';
 import './ExerciseModal.css';
 
 const ExerciseModal = ({ setOpenExerciseModal }) => {
     const handleCloseModal = () => setOpenExerciseModal(false);
+    const [isExerciseBlocked, setIsExerciseBlocked] = useState(true);
     
     return (
         <div className='exercise-modal-overlay'>
             <div className='exercise-modal'>
                 <CloseIcon handleCloseModal={handleCloseModal}/>
-                <h2>Ejercicio #1</h2>
-                
+                <h2 className='exercise-modal__title'>Ejercicio #1</h2>
+                {isExerciseBlocked ? 
+                <div className='exercise-modal__unblock-container'>
+                    <p className='exercise-modal__question'>¿Qué filósofo dijo la frase "pienso, luego existo"</p>
+                    <input className='exercise-modal__input' placeholder='Tu respuesta'/>
+                </div>
+                :
+                <></>
+                }
                 <span className="exercise-modal__close-text" onClick={handleCloseModal}>Cerrar</span>
             </div>
         </div>
