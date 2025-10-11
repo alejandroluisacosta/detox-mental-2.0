1851f2fc684d390487a2019c2862906ca8c67900
Alejandro <alejandroluisacosta95@gmail.com>
2025-10-11 21:14:39 +0200

    add: initial exercise modal elements and styles

--- frontend/src/Components/UnblockExerciseModal/UnblockExerciseModal.jsx
+++ frontend/src/Components/UnblockExerciseModal/UnblockExerciseModal.jsx
@@ -2,8 +2,17 @@ import './UnblockExerciseModal.css';
 
 const UnblockExerciseModal = () => {
     return (
-        <div className='unblock-exercise-modal'>
-            <h1>You're getting a programming job</h1>
+        <div className='unblock-exercise-modal-overlay'>
+            <div className='unblock-exercise-modal'>
+                <h2>Ejercicio #1</h2>
+                <p>Escribe cuáles son los 2-3 pensamientos que más te frenan a la hora de lograr lo que quieres (no me alcanza el tiempo, ahora no me provoca, es muy difícil...). Pueden ser más de 3.</p>
+                <p>Detállalos bien. Puedes empezar escribiendo los patrones que suelas ver en ti (ej.: «no puedo hacer X porque no soy inteligente», «nunca conseguiré lo que quiero porque mi crianza me dejó dañado», «todo lo que hago, lo hago mal», la voz de tus padres criticándote en lugar de alentarte, preocupaciones por tu salud física o mental, por falta de éxitos, escenarios imaginarios negativos que reaparecen constantemente en tu mente, etc.).</p>
+                <p>Luego, agrupa los que provengan de la misma raíz. Por ejemplo: pensar que haces todo mal y el juicio de tus padres. La falta de éxito y las dudas sobre tu inteligencia o tu competencia. La preocupación por tu salud y escenarios imaginarios de accidentes donde te haces daño.</p>
+                <p>Si escribes 9 patrones de pensamientos recurrentes y ves que tres de ellos están enraizados en tu falta de éxitos, agrúpalos en una misma categoría llamada «ambiciones» o «falta de éxitos».</p>
+                <p>Al finalizar el curso te darás cuenta de que la mayoría de tus PQAs provienen de unas pocas causas principales. Conocer estas causas es información invaluable para empezar a trabajar en ellos.</p>
+                <p>Invierte al menos 20 minutos en este ejercicio. Si puedes llegar más, mejor. Los primeros minutos serán los más complicados y aburridos, pero cuando las ideas empiecen a fluir, te costará detenerte.</p>
+                <p>Empieza a exprimir tu mente y ten los resultados de esta actividad a la mano por el resto del curso.</p>
+            </div>
         </div>
     )
 }
