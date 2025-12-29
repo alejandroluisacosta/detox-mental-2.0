b77546c317a3a62cf5f7e32d1dc3ec89936d1e99
Alejandro <alejandroluisacosta95@gmail.com>
2025-12-29 22:29:00 +0100

    fix: chatController for correct fetch of an LLM response

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -1,35 +1,24 @@
 import { InferenceClient } from "@huggingface/inference";
 
-const client = new InferenceClient(process.env.HF_TOKEN);
+
 
 export const chatController = async (req, res) => {
-  const userMessage = req.body.message;
-
-  if (!userMessage) {
-    return res.status(400).json({ error: "Missing 'message' in request body" });
-  }
-
+  const client = new InferenceClient(process.env.HF_TOKEN);
   try {
-    const response = await axios({
-      url: "https://router.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf",
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.HF_API_KEY}`,
-        "Content-Type": "application/json"
-      },
-      data: {
-        inputs: userMessage,
-      }
+    // const { message } = req.body;
+    const result = await client.chatCompletion({
+      model: "meta-llama/Llama-3.1-8B-Instruct:novita",
+      messages: [
+        {
+          role: "user",
+          content: "Why is the sky blue?"
+        }
+      ]
     });
-
-    const output = response.data[0]?.generated_text || "No response generated";
-
-    res.json({ response: output });
-
-  } catch (error) {
-    console.error("HF error:", error.response?.data || error.message || error);
-    res.status(500).json({
-      error: error.response?.data || error.message || "Unknown HF error"
-    });
+    console.log(res.json(result));
+    return res.json({ reply: result.generated_text });
+  } catch (err) {
+    console.error("HF ERROR:", err);
+    return res.status(500).json({ error: err.response?.data || err.message || "Unknown HF error" });
   }
 };
