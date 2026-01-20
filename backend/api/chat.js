437c0a3028f52e732f7046632bd9b1e27ff3189d
Alejandro <alejandroluisacosta95@gmail.com>
2026-01-20 22:48:19 +0100

    hotfix: last attempt to make the CORS thing work

--- backend/api/chat.js
+++ backend/api/chat.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://www.detoxmental.es");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Vary", "Origin");
+  res.setHeader('Access-Control-Allow-Credentials', 'true',);  
 
   // Preflight
   if (req.method === "OPTIONS") {
