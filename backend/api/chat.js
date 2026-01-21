88232a3e19d3aee6a3edde22aea85a73a30aec5b
Alejandro <alejandroluisacosta95@gmail.com>
2026-01-21 22:09:15 +0100

    hotfix: update Vercel deployment URL

--- backend/api/chat.js
+++ backend/api/chat.js
@@ -11,7 +11,6 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://www.detoxmental.es");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader('Access-Control-Allow-Credentials', 'true',);  
 
   // Preflight
   if (req.method === "OPTIONS") {
